b5782940efe0e2c85283975bb69e15ca561bc165
Maclin Phil <philmaclin@gmail.com>
2016-03-04 15:54:19 -0600

    Adds Song Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :songs
   resources :reviews
   resources :performances
   resources :venues
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304213909) do
+ActiveRecord::Schema.define(version: 20160304215309) do
 
   create_table "performances", force: :cascade do |t|
     t.integer  "rating"
@@ -37,6 +37,18 @@ ActiveRecord::Schema.define(version: 20160304213909) do
 
   add_index "reviews", ["performance_id"], name: "index_reviews_on_performance_id"
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "artist"
+    t.string   "title"
+    t.string   "genre"
+    t.text     "lyrics"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "songs", ["user_id"], name: "index_songs_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
