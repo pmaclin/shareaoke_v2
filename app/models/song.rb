cbe22571495769b62818cee341bbdb586e08f732
Maclin Phil <philmaclin@gmail.com>
2016-03-04 16:30:26 -0600

    Adds Validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :user
 
   has_many :performances
+
+  validates :artist, :presence => true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,6 @@ class Venue < ActiveRecord::Base
   belongs_to :user
 
   has_many :users
+
+  validates :name, :presence => true, :uniqueness => {:scope => :city}
 end
