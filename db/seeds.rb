a8cb7ac23e0dd6999717cfdb9be8e2b53f2e474e
Maclin Phil <philmaclin@gmail.com>
2016-03-04 18:08:12 -0600

    Adds Seeds File and other stuff
    Populates Seed File
    Adds before_action in App Controller
    Adds ‘Stagename’ field to Devise Registrations/new
    Add permitted parameters in App Controller (‘stagename’)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.create!([
+  {title: "Work", artist: "Rihanna feat. Drake", genre: "Hot 100", lyrics: ""},
+  {title: "Love Yourself", artist: "Justin Bieber", genre: "Hot 100", lyrics: ""},
+  {title: "Stressed Out", artist: "Twenty One Pilots", genre: "Hot 100", lyrics: ""},
+  {title: "Sorry", artist: "Justin Bieber", genre: "Hot 100", lyrics: ""},
+  {title: "My House", artist: "Flo Rida", genre: "Hot 100", lyrics: ""},
+  {title: "Pillowtalk", artist: "Zayn", genre: "Hot 100", lyrics: ""},
+  {title: "Me, Myself & I", artist: "G-Eazy x Bebe Rexha", genre: "Hot 100", lyrics: ""},
+  {title: "Hello", artist: "Adele", genre: "Hot 100", lyrics: ""},
+  {title: "Cake By The Ocean", artist: "DNCE", genre: "Hot 100", lyrics: ""},
+  {title: "Roses", artist: "The Chainsmokers feat. Rozes", genre: "Hot 100", lyrics: ""},
+  {title: "Hands To Myself", artist: "Selena Gomez", genre: "Hot 100", lyrics: ""},
+  {title: "7 Years", artist: "Lukas Graham", genre: "Hot 100", lyrics: ""},
+  {title: "Down In The DM", artist: "Yo Gotti Featuring Nicki Minaj", genre: "Hot 100", lyrics: ""},
+  {title: "One Call Away", artist: "Charlie Puth", genre: "Hot 100", lyrics: ""},
+  {title: "I Took A Pill In Ibiza", artist: "Mike Posner", genre: "Hot 100", lyrics: ""},
+  {title: "Stiches", artist: "Shawn Mendes", genre: "Hot 100", lyrics: ""},
+  {title: "Don't", artist: "Bryson Tiller", genre: "Hot 100", lyrics: ""},
+  {title: "When We Were Young", artist: "Adele", genre: "In The Night", lyrics: ""},
+  {title: "Same Old Love", artist: "Selena Gomez", genre: "Hot 100", lyrics: ""},
+  {title: "Hotline Bling", artist: "Drake", genre: "Hot 100", lyrics: ""},
+  {title: "The Hills", artist: "The Weekend", genre: "Hot 100", lyrics: ""},
+  {title: "Hide Away", artist: "Daya", genre: "Hot 100", lyrics: ""},
+  {title: "What Do You Mean?", artist: "Justin Bieber", genre: "Hot 100", lyrics: ""}
+])
+Venue.create!([
+  {name: "Lincoln Karaoke", address: "5526 N. Lincoln Avenue", city: "Chicago", description: "Karaoke bar with private rooms featuring multiple mikes, TVs & sofas, plus cocktails & bar eats.", user_id: 1},
+  {name: "Brando's Speakeasy", address: "343 S Dearborn St, Chicago, IL 60604", city: "Chicago", description: "Intimate lounge/club delivering drinks, karaoke & dancing in a vintage space in a historic building.", user_id: 1},
+  {name: "Murasaki Saki Lounge", address: "211 E. Ontario", city: "Chicago", description: "Swanky Japanese lounge with a big sake & spirits list, a small-plates menu & a karaoke room.", user_id: 1},
+  {name: "Sakura Karaoke Lounge", address: "234 W. Cermak Rd. ", city: "Chicago", description: "Trendy subterranean karaoke lounge with cocktails, light bites & dancing amid TV displays & sofas. Basement.", user_id: 1},
+  {name: "Trader Todd's", address: "3216 N. Sheffield Ave", city: "Chicago", description: "Festive karaoke bar with a beer garden, seafood-filled bar menu & tropical cocktails.", user_id: 1},
+  {name: "Alice's Lounge", address: "3556 W. Belmont", city: "Chicago", description: "They have cider and even gluten free beer (New Grist).", user_id: 1}
+])
