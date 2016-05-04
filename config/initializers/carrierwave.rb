b230fa5776576332975f41c2c9ab97a232ef306f
Maclin Phil <philmaclin@gmail.com>
2016-05-04 00:43:29 -0500

    Updating application.yml
    S3_BUCKET_NAME

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]                        # required
   }
-  config.fog_directory = ENV["AWS_S3_BUCKET"]                          # required
+  config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]                          # required
 
 end
