a1ec934d7099b37c28f672ab610200b2eb66253c
Maclin Phil <philmaclin@gmail.com>
2016-05-06 22:11:44 -0500

    Edits assets.rb

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require 'fog'
+require 'fog/aws'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
