badfc477f9b8862453022b0ff7749bf86907a85e
Maclin Phil <philmaclin@gmail.com>
2016-05-12 13:30:42 -0500

    reloads figaro, new application.yml file

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,8 +9,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
-  include CarrierWave::MimeTypes
-  process :set_content_type
+  # include CarrierWave::MimeTypes
+  # process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
